50326ba534d8c6126eb643f70ee000916ebcd613
Shivashankar07 <52339958+Shivashankar07@users.noreply.github.com>
2022-10-13 22:26:52 +0530

    Add files via upload

---
+++ async-parallel.js
@@ -0,0 +1,44 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+const async = require("async");
+
+function aa(callback) {
+  setTimeout(function () {
+    console.log("Task One");
+    callback(null, 1);
+  }, 200);
+}
+function bb(callback) {
+  setTimeout(function () {
+    console.log("Task Two");
+    callback(null, 2);
+  }, 100);
+}
+
+async.parallel([aa, bb], function (err, results) {
+  console.log(results);
+  // the results array will equal [1, 2] even though
+  // the second function had a shorter timeout.
+});
+
+// an example using an object instead of an array
+async.parallel(
+  {
+    task1: function (callback) {
+      setTimeout(function () {
+        console.log("Task One");
+        callback(null, 1);
+      }, 200);
+    },
+    task2: function (callback) {
+      setTimeout(function () {
+        console.log("Task Two");
+        callback(null, 2);
+      }, 100);
+    },
+  },
+  function (err, results) {
+    console.log(results);
+    // results now equals to: { task1: 1, task2: 2 }
+  }
+);
